86b42520699b443d10996b1b928e2ff380d9a2eb
Artem O <dev@mail.ru>
2018-05-14 18:51:22 +0700

    wss

--- main.go
+++ main.go
@@ -5,27 +5,29 @@ import (
 	"net/http"
 )
 
-// func serveHome(w http.ResponseWriter, r *http.Request) {
-// 	log.Println(r.URL)
-// 	if r.URL.Path != "/" {
-// 		http.Error(w, "Not found", http.StatusNotFound)
-// 		return
-// 	}
-// 	if r.Method != "GET" {
-// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-// 		return
-// 	}
-// 	http.ServeFile(w, r, "home.html")
-// }
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/test" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
 
 func main() {
 
 	hub := newHub()
 	go hub.run()
+	http.HandleFunc("/test", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 	err := http.ListenAndServe(":8081", nil)
+	//err := http.ListenAndServeTLS("host:8081", "./fullchain1.pem", "./privkey1.pem", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
